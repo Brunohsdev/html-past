61a83735ee80c641470cba4ec39977b25fb2e118
Brunohsdev <bhas795002@gmail.com>
2025-04-01 09:10:48 -0300

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let botão ligar/desligar = 0;
+let trocar de canal = 1; 
+let volume = 0;
+const vida-da-tv = 1;
+
+
+let resposta = prompt("Deseja ligar a tv?")
+if (resposta == "s"){
+  alert('Você ligou a tv');
+  alert('status da tv: ');
+  
+    
+while (vida-da-tv=1 & ){
+  
+      
+    }
+
+
+
+  
+}
